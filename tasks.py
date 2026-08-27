77454d50406245793cf021b973c1e4080b1a5476
Ameer Khan <ameer.khan.linkedin@gmail.com>
2026-08-27 19:20:49 -0400

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -21,7 +21,7 @@ class Task:
 
 class RecurringTask(Task):
     def __init__(self, title, priority="medium", done=False, frequency="d"):
-        super().__init__(title, priority, done)
+        super().__init__(title, priority, done)             #⬅️ It is necessary 2 inherit parameters from super class.
         self.frequency = frequency
 
     def describe(self):
