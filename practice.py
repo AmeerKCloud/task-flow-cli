79cc59a7e674a206e3cbe0ac30bb0008c7c798b7
Ameer Khan <ameer.khan.linkedin@gmail.com>
2026-09-04 21:45:02 -0400

    Update practice.py

--- practice.py
+++ practice.py
@@ -2,3 +2,12 @@ class SampleClass:
     def __init__(self, title, done=False):
         self.title = title
         self.done = done
+
+sample_object = SampleClass(title="This is a sample class")
+
+print(sample_object.title)
+print(sample_object.done)
+
+sample_object.done = True
+
+print(sample_object.done)
