4a1f03a7da312d8d6c857cb189498a6c1a6f7390
AmeerKCloud <ameer.khan.linkedin@gmail.com>
2026-09-06 10:17:06 -0400

    Update main.py

--- main.py
+++ main.py
@@ -110,6 +110,9 @@ while True:
             else:
                 print("You have no tasks to show.")
         elif view_tasks_type == 2:
+            for task in all_tasks:
+                if task.done == True:
+                    print(task.describe())
             pass                                        #⬅️⚠️ Currently here. Do completed tasks need to go in their own list? If no task done, then list empty, user notified.
     elif user_choice == 3:
         title_input = input("Enter task title:\n")
