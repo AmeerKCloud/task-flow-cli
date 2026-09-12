f8cfa0cae7ad86706ddd06bbc1c576a69144b4d0
Ameer Khan <ameer.khan.linkedin@gmail.com>
2026-09-11 22:37:51 -0400

    Update main.py

--- main.py
+++ main.py
@@ -139,7 +139,7 @@ while True:
             print_summary(all_tasks=all_tasks)
         elif view_stat_type == 2:
             incomplete_tasks = filter_tasks(tasks=all_tasks)
-            for task in incomplete_tasks:
+            for task in incomplete_tasks[:3]:
                 print(task.describe())                                     #⬅️ prints ur next 3 incomplete tasks.
         elif view_stat_type == 3:
             completed_tasks = filter_tasks(tasks=all_tasks, done=True)
