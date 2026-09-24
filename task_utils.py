94e34eadfa08bd0bf2e06599120d08bceeeb8529
Ameer Khan <ameer.khan.linkedin@gmail.com>
2026-09-24 19:14:04 -0400

    updates

--- task_utils.py
+++ task_utils.py
@@ -55,7 +55,10 @@ def format_task_line(task, task_index):
     "next 3" preview, etc.) instead of writing the same formatting/f-string logic 
     over and over in each menu option."""
 
-    return f"{task_index + 1}. {task.describe()}"
+    if task.done == False:
+        return f"[ ]{task_index + 1}. {task.describe()}"
+    else:
+        return f"[]{task_index + 1}. {task.describe()}"
 
 def random_motivational_quote():
     rand_quotes = ["You got this!!", "One step at a time!", "Do better!", "Don't quit now!", "You're almost there!", "Quitting is for losers"]
