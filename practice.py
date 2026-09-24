0fe1fb498fc33f7b956c0b8c43280c7c3a0a45ec
Ameer Khan <ameer.khan.linkedin@gmail.com>
2026-09-24 11:26:02 -0400

    updates

--- practice.py
+++ practice.py
@@ -1,13 +1,4 @@
-class SampleClass:
-    def __init__(self, title, done=False):
-        self.title = title
-        self.done = done
+import random
 
-sample_object = SampleClass(title="This is a sample class")
-
-print(sample_object.title)
-print(sample_object.done)
-
-sample_object.done = True
-
-print(sample_object.done)
+def shuffle_list(list):
+    random.shuffle(list)
